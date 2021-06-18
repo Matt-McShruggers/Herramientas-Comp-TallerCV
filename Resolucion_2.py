922ba93d8dc6a7a5b55a70269d8124afff9eb3f3
Matt-McShruggers <71412214+Matt-McShruggers@users.noreply.github.com>
2021-06-17 20:13:46 -0500

    Añadir la cantidad de veces a repetir el codigo

--- Resolucion_2.py
+++ Resolucion_2.py
@@ -1,9 +1,11 @@
 def NotaFinal():
-    parcial1 = float(input("Nota de primer parcial: "))
-    parcial2 = float(input("Nota de segundo parcial: "))
-    nota_taller = float(input("Nota de taller: "))
-    taller = nota_taller*0.8
-    nota_proyecto = float(input("Nota del proyecto: "))
-    proyecto = nota_proyecto*1.2
-    nota_final = (parcial1 + parcial2 + taller + proyecto)/4
-    print("La nota final de este curso es: ",nota_final) 
+    pregunta = int(input("¿Cuantas veces se va a repetir este codigo? "))
+    for i in range(pregunta):
+        parcial1 = float(input("Nota de primer parcial: "))
+        parcial2 = float(input("Nota de segundo parcial: "))
+        nota_taller = float(input("Nota de taller: "))
+        taller = nota_taller*0.8
+        nota_proyecto = float(input("Nota del proyecto: "))
+        proyecto = nota_proyecto*1.2
+        nota_final = (parcial1 + parcial2 + taller + proyecto)/4
+        print("La nota final de tu curso es: ",nota_final) 
